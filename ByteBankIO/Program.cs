79651e05eb3743a36ac34a85e8d859bc9515d7d9
Mauro Lopes <mauro.slopes06@gmail.com>
2025-03-11 18:24:14 -0300

    Lendo arquivo de texto em UTF8

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,10 +1,40 @@
 ﻿using ByteBankIO;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        string enderecoDoArquivo = "contas.txt";
+
+        int numeroDeBytesLidos = -1;
+
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; // 1kb
+
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
 
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+
+        var utf8 = new UTF8Encoding();
+
+        string texto = utf8.GetString(buffer);
+
+        Console.Write(texto);
+
+        //foreach (byte b in buffer)
+        //{
+        //    Console.Write(b);
+        //    Console.Write(" ");
+        //}
     }
 }
